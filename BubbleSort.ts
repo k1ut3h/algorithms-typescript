8ea8d652b179a40da3378e3729e4d8607d6f733d
k1ut3h <hardikedu811@gmail.com>
2023-05-09 09:59:27 +0530

    added queue

--- BubbleSort.ts
+++ BubbleSort.ts
@@ -1,21 +1,19 @@
 function BubbleSort(arr:number[]):number[]{
-  let bound = arr.length;
   let i = 0;
-  while(i<bound){
-    for(let j=0; j<arr.length; ++j){
+  while(i<arr.length){
+    for(let j=0; j<arr.length - 1 -i; ++j){
       if(arr[j+1]<arr[j]){
         let tmp = arr[j+1];
         arr[j+1] = arr[j];
         arr[j] = tmp;
       }
     }
-    bound-=1;
     i+=1;
   }
   return arr;
 }
 
-let arr = [23,1,54,2,7,4,3];
+let arr = [6,5,4,3,2,1,0];
 console.log("Given Array: ", arr);
 console.time("test_timer");
 console.log("Sorted Array", BubbleSort(arr));
